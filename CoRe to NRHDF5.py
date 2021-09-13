fd13861f13817c933f4c751bedec77416b610309
Sanika Khadkikar <61140414+sanikakhadkikar@users.noreply.github.com>
2021-09-13 12:48:54 -0400

    Step 1 : File conversion

---
+++ CoRe to NRHDF5.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import h5py
+import numpy as np
+import romspline
+
+
+# In[2]:
+
+
+#File selection 
+gw_core = h5py.File('/home/sanikawork/Desktop/Sathya/CoRe/THC_0001/R01/data.h5','r')
+keys = list(gw_core['rh_22'].keys())
+gw_core['rh_22'].keys()
+
+
+# In[3]:
+
+
+#Required inputs 
+file_name = 'THC_0001_conv.h5'
+l_max = 4
+
+df = h5py.File(file_name, 'a')
+df.attrs['NR-group'] = 'CoRe'
+df.attrs['type'] = 'non spinning'
+df.attrs['name'] = 'BHBlp_1.250_1.250_0.00_0.00_0.050'
+df.attrs['object1'] = 'NS'
+df.attrs['object2'] = 'NS'
+df.attrs['mass1'] = 1.25
+df.attrs['mass2'] = 1.25
+df.attrs['eta'] = 0.25
+df.attrs['spin1x'] = 0
+df.attrs['spin1y'] = 0
+df.attrs['spin1z'] = 0
+df.attrs['spin2x'] = 0
+df.attrs['spin2y'] = 0 
+df.attrs['spin2z'] = 0
+df.attrs['LNhatx'] = 0 
+df.attrs['LNhaty'] = 0
+df.attrs['LNhatz'] = 1
+df.attrs['nhatx'] = 1
+df.attrs['nhaty'] = 0 
+df.attrs['nhatz'] = 0
+df.attrs['f_lower_at_1MSUN'] = 0.0498819129216
+
+
+# In[4]:
+
+
+for l in range(l_max+1):
+    for m in range(0,l+1,1):
+        count = 0 
+        temp_r = np.array(gw_core['rh_22'][keys[count]][:,1])
+        temp_i = np.array(gw_core['rh_22'][keys[count]][:,2])
+        comp = temp_r + 1j*temp_i
+        mag = np.abs(comp)
+        df = h5py.File(file_name, 'a')
+        str_name = 'amp_l'+str(l)+'_m'+ str(m)
+        df.create_group(str_name)
+        spline = romspline.ReducedOrderSpline(np.array(gw_core['rh_22'][keys[count]][:,8]),mag,verbose=False)
+        df[str_name].create_dataset('X', data=spline.X)
+        df[str_name].create_dataset('Y', data=spline.Y)
+        df[str_name].create_dataset('deg', data=[5])
+        df[str_name].create_dataset('errors', data=spline.errors)
+        df[str_name].create_dataset('tol', data=spline.tol)
+        df.close()
+        print(str_name + ' is done')
+        
+df = h5py.File(file_name, 'a')
+df.create_group('auxillary info')
+df.close()
+
+for l in range(l_max+1):
+    for m in range(0,l+1,1):
+        count = 0 
+        temp_r = np.array(gw_core['rh_22'][keys[count]][:,1])
+        temp_i = np.array(gw_core['rh_22'][keys[count]][:,2])
+        comp = temp_r + 1j*temp_i
+        phase = np.angle(comp)
+        df = h5py.File(file_name, 'a')
+        str_name = 'phase_l'+str(l)+'_m'+ str(m)
+        df.create_group(str_name)
+        spline = romspline.ReducedOrderSpline(np.array(gw_core['rh_22'][keys[count]][:,8]),phase,verbose=False)
+        df[str_name].create_dataset('X', data=spline.X)
+        df[str_name].create_dataset('Y', data=spline.Y)
+        df[str_name].create_dataset('deg', data=[5])
+        df[str_name].create_dataset('errors', data=spline.errors)
+        df[str_name].create_dataset('tol', data=spline.tol)
+        df.close()
+        print(str_name + ' is done')
+
+
+# In[5]:
+
+
+new_file = h5py.File(file_name,'r')
+new_file.keys()
+
+
+# In[6]:
+
+
+print(keys)
+
+
+# In[7]:
+
+
+new_file.close()
+
+
+# In[ ]:
+
+
+
+
